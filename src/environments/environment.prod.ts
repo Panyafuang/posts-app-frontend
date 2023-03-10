66c4bdf0062cdc86cd5f15371b9f833b816624aa
Panyafuang <thanadilok.x@gmail.com>
2023-03-10 12:10:02 +0700

    update config for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://vast-puce-barracuda-slip.cyclic.app/api'
+  // apiUrl: 'https://vast-puce-barracuda-slip.cyclic.app/api'
+  apiUrl: "#{apiUrl}"
 }
